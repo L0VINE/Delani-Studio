ef3ad9757d8c441243547b67b7ef6e1e71a6ba61
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-06-21 14:38:20 +0300

    added functionality to what we do section

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,3 +1,51 @@
+$(function () {
+    let desp = $('#desp');
+    let devp = $('#devp');
+    let prop = $('#prop');
+    let destext = $('#destxt');
+    let devtext = $('#devtext');
+    let protext = $('#protext');
+
+    $(desp).hide();
+    $(devp).hide();
+    $(prop).hide();
+
+    //Show paragraph content
+    $('.des').on('click', function () {
+        $('.des').slideUp('slow');
+        $(desp).show(500);
+        $(destext).animate({fontWeight:'bolder',fontSize:'30px'})
+
+    });
+    $('.dev').on('click', function () {
+        $('.dev').slideUp('slow');
+        $(devp).show(500);
+        $(devtext).animate({fontWeight:'bolder',fontSize:'30px'})
+    });
+    $('.pro').on('click', function () {
+        $('.pro').slideUp('slow');
+        $(prop).show(500);
+        $(protext).animate({fontWeight:'bolder',fontSize:'30px'})
+    });
+
+    //Hide paragraph content
+    $(desp, destext).on('click', function () {
+        $('.des').slideDown();
+        $(desp).hide(500);
+        $(destext).animate({fontWeight:'normal',fontSize:'16px'})
+    });
+    $(devp, devtext).on('click', function () {
+        $('.dev').slideDown();
+        $(devp).hide(500);
+        $(devtext).animate({fontWeight:'normal',fontSize:'16px'})
+    });
+    $(prop, protext).on('click', function () {
+        $('.pro').slideDown();
+        $(prop).hide(500);
+        $(protext).animate({fontWeight:'normal',fontSize:'16px'})
+    })
+
+
 //Form Validation
         $("form#form").on('submit',function(event){
             event.preventDefault();
@@ -6,10 +54,11 @@
             let message = $("textarea#message").val();
 
             if ($("input#name").val() && $("input#email").val()){
-                alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you shortly...cheers");
+                alert ("Hey " + name + ", Thanks for reaching out to us.We will get back to you.");
             }
             else {
                 alert("Please provide your correct name and email!");
             }
 
         });
+    });
