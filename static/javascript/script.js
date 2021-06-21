fbb157f1fac54a42c4dc42ef0b177ba3a1d7657f
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-06-21 14:19:18 +0300

    added functionality to the form

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,17 +1,15 @@
-$("form#form1").on('submit',function(event){
-    event.preventDefault();
-    var name = $("input#name").val();
-    var email = $("input#email").val();
-    var message = $("textarea#message").val();
+//Form Validation
+        $("form#form").on('submit',function(event){
+            event.preventDefault();
+            let name = $("input#name").val();
+            let email = $("input#email").val();
+            let message = $("textarea#message").val();
 
-    if ($("input#name").val() && $("input#email").val() && $("input#message").val() ){
-        alert ("Hello " + name + ", Thank you for reaching out, we'll get back to you shortly...");
-    }
-    else if($("input#name").val() && $("input#email").val()){
-        alert("Thank you for reaching ou to us. If you wish to leave a message please type in on the message section..");
-    }
-   else{
-     alert("Please provide correct name or email")
-   }
-});
+            if ($("input#name").val() && $("input#email").val()){
+                alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you shortly...cheers");
+            }
+            else {
+                alert("Please provide your correct name and email!");
+            }
 
+        });
